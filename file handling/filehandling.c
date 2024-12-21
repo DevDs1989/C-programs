e76a92fc76c9d014856b3031ecab4a70e0ea1e78
DevDs1989 <177749438+DevDs1989@users.noreply.github.com>
2024-12-21 22:48:42 +0530

    Update filehandling.c

--- file handling/filehandling.c
+++ file handling/filehandling.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-
-  FILE* file; 
-  file = fopen("hello.txt", "r");
-  if(file = NULL){
-    printf("no such file");
-  }
-  else{
-    printf("file opened");
-  }
-
+    FILE *file = fopen("hello.txt", "w");
+    if (file == NULL) {
+        printf("Error opening file");
+        exit();
+    }
+    fprintf(file, "New text in file");
+    fclose(file);
+    printf("File created and text written successfully.\n");
+    return 0;
 }
