a4a5dc45e3b1257efb02d96e99f2f3bf833f10ab
DevDs1989 <177749438+DevDs1989@users.noreply.github.com>
2024-12-21 22:50:39 +0530

    Update filehandling.c

--- file handling/filehandling.c
+++ file handling/filehandling.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 int main() {
-    FILE *file = fopen("hello.txt", "r");
+    FILE *file = fopen("newfile.txt", "r");
     if (file == NULL) {
-        printf("Error opening file");
-        exit();
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
     }
-    printf("Reading file content character by character:\n");
-    char ch;
-    while ((ch = fgetc(file)) != EOF) {
-        putchar(ch);
+    printf("Reading file content line by line:\n");
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
     }
     fclose(file);
+    printf("\nFile read completed.\n");
     return 0;
 }
